244602f9754e9d7adff17f6b1fbb4a777cde15bc
Saurezsh <msuarez@unsa.edu.pe>
2024-11-24 05:48:05 -0500

    Configurar vistas, URLs y plantillas para libros y reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'reviews/book_list.html', {'books': books})
+
+def book_reviews(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'reviews/book_reviews.html', {'book': book})
