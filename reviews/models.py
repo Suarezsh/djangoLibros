00561014420857bf3c2bde3e765c22b7ee664d01
Saurezsh <msuarez@unsa.edu.pe>
2024-11-24 05:43:52 -0500

    se agrega modelos book y review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    user = models.CharField(max_length=255) 
+    comment = models.TextField()
+    rating = models.PositiveIntegerField() 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review for {self.book.title} by {self.user}"
