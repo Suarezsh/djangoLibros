bf70872711f8d0f8b8c73306edc383bdc9ffb6af
Saurezsh <msuarez@unsa.edu.pe>
2024-11-24 05:57:07 -0500

    se agregO campo 'genre' al modelo Book

--- reviews/models.py
+++ reviews/models.py
@@ -4,9 +4,11 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     published_date = models.DateField()
+    genre = models.CharField(max_length=100, default="General")  # Nuevo campo
 
     def __str__(self):
         return self.title
+
 
 
 class Review(models.Model):
