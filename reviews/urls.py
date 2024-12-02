a8097e0e2dd923419efd07e56f02ef1fd28fb4ab
Saurezsh <msuarez@unsa.edu.pe>
2024-12-02 07:57:12 -0500

    Creacion de fomrularios para añadir mediante la vista

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
-    path('books/<int:book_id>/', views.book_reviews, name='book_reviews'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('add_review/', views.add_review, name='add_review'),
+    path('book_reviews/<int:book_id>/', views.book_reviews, name='book_reviews'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import Book
+from .forms import BookForm, ReviewForm
+from django.shortcuts import get_object_or_404
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'reviews/book_list.html', {'books': books})
+    book_form = BookForm()
+    review_form = ReviewForm()
+    return render(request, 'reviews/book_list.html', {
+        'books': books,
+        'book_form': book_form,
+        'review_form': review_form,
+    })
 
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+    return JsonResponse({'success': False, 'errors': form.errors})
+
+def add_review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+    return JsonResponse({'success': False, 'errors': form.errors})
 def book_reviews(request, book_id):
-    book = Book.objects.get(id=book_id)
-    return render(request, 'reviews/book_reviews.html', {'book': book})
+    book = get_object_or_404(Book, id=book_id)
+    reviews = book.reviews.all()
+    reviews_data = [
+        {
+            "user": review.user,
+            "comment": review.comment,
+            "rating": review.rating,
+            "created_at": review.created_at.strftime("%d/%m/%Y %H:%M"),
+        }
+        for review in reviews
+    ]
+    return JsonResponse({"book_title": book.title, "reviews": reviews_data})
